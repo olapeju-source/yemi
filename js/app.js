2d8f27f6ff94545e9a1ea3d7f97abc809a858b8c
olapeju-source <oladokunolapeju94@gmail.com>
2021-06-11 16:47:50 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const reviews = [
+    {
+      id: 1,
+      img:"img/cap-2.jpg"
+     
+    },
+    {
+        id: 2,
+        img:"img/cap-2.jpg"
+       
+    },
+    {
+        id: 1,
+        img:"img/cap-2.jpg"
+       
+    },
+    {
+        id: 1,
+        img:"img/cap-2.jpg"
+       
+      }
+    
+  ];
