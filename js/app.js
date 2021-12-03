5c1f886d1e94fedc0c87e41cba279f01586e2a24
olapeju-source <oladokunolapeju94@gmail.com>
2021-12-03 07:15:38 -0800

    first commit

--- js/app.js
+++ js/app.js
@@ -5,8 +5,31 @@ bar.addEventListener("click", () => {
 //toggle
 navLink.classList.add("show-link");
   });
-
-  time.addEventListener("click", () => {
-    //toggle
-    navLink.classList.remove("show-link");
+time.addEventListener("click", () => {
+      navLink.classList.remove("show-link");
       });
+    
+
+      window.addEventListener("scroll", function() {
+        if (window.scrollY > 50) {
+          document.querySelector("header").style.boxShadow =
+            "9px 9px 10px -12px rgb(26, 25, 25)";
+       
+        } else {
+          document.querySelector("header").style.boxShadow=
+            "none";
+       
+        }
+      });
+
+
+      var arrowUp = document.querySelector(".arrowup");
+      window.addEventListener("scroll", clickArrow);
+      
+      function clickArrow() {
+        if (window.scrollY > 50) {
+          arrowUp.style.display = "block";
+        } else {
+          arrowUp.style.display = "none";
+        }
+      } 
